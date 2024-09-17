2b963d1e3497b44c03f59f56c80619578dd6fcaf
123 <huanganli@autowise.ai>
2024-09-17 23:12:15 +0800

    add tuner

---
+++ XTuner/env/create_env.sh
@@ -0,0 +1,34 @@
+# 创建虚拟环境
+conda create -n xtuner0121 python=3.10 -y
+
+# 激活虚拟环境（注意：后续的所有操作都需要在这个虚拟环境中进行）
+conda activate xtuner0121
+
+# 安装一些必要的库
+conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=12.1 -c pytorch -c nvidia -y
+# 安装其他依赖
+pip install transformers==4.39.3
+pip install streamlit==1.36.0
+mkdir -p /root/InternLM/code
+
+cd /root/InternLM/code
+
+git clone -b v0.1.21  https://github.com/InternLM/XTuner /root/InternLM/code/XTuner
+
+# 创建一个目录，用来存放微调的所有资料，后续的所有操作都在该路径中进行
+mkdir -p /root/InternLM/XTuner
+
+cd /root/InternLM/XTuner
+
+mkdir -p Shanghai_AI_Laboratory
+
+ln -s /root/share/new_models/Shanghai_AI_Laboratory/internlm2-chat-1_8b $(pwd)/Shanghai_AI_Laboratory/internlm2-chat-1_8b
+
+
+
+
+
+
+
+
+
